fe427543629fd27b6ef9a2cb809684c46e477424
RyanEWard <30811198+RyanEWard@users.noreply.github.com>
2017-08-29 14:01:00 -0600

    Return json by default to html requests.

--- TextClassification/App_Start/WebApiConfig.cs
+++ TextClassification/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace TextClassification
@@ -10,6 +12,12 @@ namespace TextClassification
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+                .Add(new RequestHeaderMapping("Accept",
+                              "text/html",
+                              StringComparison.InvariantCultureIgnoreCase,
+                              true,
+                              "application/json"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
